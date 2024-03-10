0277d3a05698b78e9980ee5995961024ee656689
Joel Cannon <joelcannon@mac.com>
2024-03-09 19:04:29 -0600

    fid delete routes

--- routes/temple.routes.js
+++ routes/temple.routes.js
@@ -2,14 +2,28 @@ const templeRouter = require("express").Router();
 const templeController = require("../controllers/temple.controller.js");
 
 templeRouter.get("/", templeController.getAllTemples);
+// #swagger.description = 'get all Temples'
+// #swagger.tags = ['temples']
+
 templeRouter.get("/:temple_id", templeController.getTempleById);
+// #swagger.description = 'get a single Temple by temple_id'
+// #swagger.tags = ['temples']
 
 templeRouter.post("/", templeController.createTemple);
+// #swagger.description = 'add a new Temple'
+// #swagger.tags = ['temples']
 
-templeRouter.put("/temples/:id", templeController.updateTempleById);
+templeRouter.put("/:id", templeController.updateTempleById);
+// #swagger.description = 'update a Temple by id'
+// #swagger.tags = ['temples']
 
-templeRouter.delete("/temples/:id", templeController.deleteTempleById);
-templeRouter.delete("/temples", templeController.deleteAllTemples);
+templeRouter.delete("/:id", templeController.deleteTempleById);
+// #swagger.description = 'delete a single Temple by id'
+// #swagger.tags = ['temples']
+
+templeRouter.delete("/", templeController.deleteAllTemples);
+// #swagger.description = 'delete all Temples'
+// #swagger.tags = ['temples']
 
 templeRouter.use(templeController.errorHandler);
 

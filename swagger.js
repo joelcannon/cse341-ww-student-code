5fd4bd2f981cb729ce69fb37a38765224aa8d32b
Joel Cannon <joelcannon@mac.com>
2024-03-09 22:26:12 -0600

    chain swagger to then run server.js

--- swagger.js
+++ swagger.js
@@ -16,5 +16,8 @@ const routes = ['./routes/index.js']
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
-swaggerAutogen(outputFile, routes, doc)
+// swaggerAutogen(outputFile, routes, doc)
 console.log('Running swagger script...')
+swaggerAutogen(outputFile, routes, doc).then(() => {
+  require('./server.js')
+})

9d37c6cfd6f05a666811838a62f8fee0f5b108e4
Joel Cannon <joelcannon@mac.com>
2024-03-11 18:59:42 -0500

    upgrade swagger to open-api 3.0

--- swagger.js
+++ swagger.js
@@ -1,4 +1,4 @@
-const swaggerAutogen = require('swagger-autogen')()
+const swaggerAutogen = require('swagger-autogen')({ openapi: '3.0.0' })
 
 const doc = {
   info: {
@@ -6,22 +6,21 @@ const doc = {
     description:
       'BYUI CSE341 Week 2: API Documentation for REST-client project',
   },
-  host: process.env.HOST || 'localhost:8080',
-  // basePath: process.env.PORT ? `:${process.env.PORT}` : ':8080',
-  schemes: [process.env.SWAGGER_SCHEME || 'http'],
+  servers: [
+    // Used in place of 'schemes', 'basePath', etc.
+    // Look into documentation on their GitHub repo's.
+    {
+      // The first url set will be your default.
+      url: 'http://localhost:8080',
+      description: 'localhost:8080',
+    },
+    {
+      // This is another option that can be selected to connect through.
+      url: 'https://cse341-contact-list.onrender.com/',
+      description: 'Render URL',
+    },
+  ],
 }
-console.log(
-  'env: ',
-  process.env.SWAGGER_SCHEME,
-  process.env.HOST,
-  // process.env.PORT,
-)
-console.log(
-  'local: ',
-  doc.schemes,
-  doc.host,
-  // doc.basePath
-)
 
 const outputFile = './swagger-output.json'
 const routes = ['./routes/index.js']
@@ -31,6 +30,8 @@ root file where the route starts, such as index.js, app.js, routes.js, etc ... *
 
 // swaggerAutogen(outputFile, routes, doc)
 console.log('Running swagger script...')
-swaggerAutogen(outputFile, routes, doc).then(() => {
-  require('./server.js')
-})
+swaggerAutogen(outputFile, routes, doc)
+
+// swaggerAutogen(outputFile, routes, doc).then(() => {
+//   require('./server.js')
+// })
